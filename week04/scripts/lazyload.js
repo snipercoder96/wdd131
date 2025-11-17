6527d9b9919e49771cfd654a6283b8ba12ae2220
snipercoder96 <kmbembakiyindou@byupathway.edu>
2025-11-17 15:31:42 +0200

    Added Changes to webpage

--- week04/scripts/lazyload.js
+++ week04/scripts/lazyload.js
@@ -4,7 +4,16 @@ document.getElementById("currentyear").textContent = currentYear;
 const lastModified = document.lastModified;
 document.getElementById("lastModified").textContent = lastModified;
 
+
+/* Playing around with h1 */
 const h1 = document.querySelector("h1");
 h1.textContent = "LazyLoading Images Demonstration";
 h1.style.textAlign = "center";
-h1.style.color = "rgb(74, 158, 0)";
+h1.style.color = "rgba(122, 255, 5, 1)";
+h1.style.backgroundColor = "#000";
+h1.style.padding = "20px 0";
+h1.style.margin = "0 0 30px 0";
+
+
+const title = document.querySelector("title");
+title.textContent = "Lazy loading images";
